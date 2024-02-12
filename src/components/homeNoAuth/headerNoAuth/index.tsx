f00a0ab118517493ed428f62658045c45538a5ce
Marcelo Leal <andradefj13@hotmail.com>
2024-02-12 10:47:26 -0300

    update
    correção na logica do bootstrap que na versão 13 do next não funciona mais da mesma forma

--- src/components/homeNoAuth/headerNoAuth/index.tsx
+++ src/components/homeNoAuth/headerNoAuth/index.tsx
@@ -1,12 +1,22 @@
+import { Button, Container } from 'reactstrap';
 import style from './style.module.scss';
-
+import Link from 'next/link';
 const HeaderNoAuth = () => {
     return (
         <>
             <div className={style.ctaSection}>
-                <img src="/homeNoAuth/iconSearch.svg" alt="logoCta" className={style.imgCta} />
+                <img src="/homeNoAuth/logoCta.png" alt="logoCta" className={style.imgCta} />
                 <p>Se cadastre para ter acesso aos cursos</p>
+                <img src="/homeNoAuth/logoCta.pnp" alt="logoCta" />
             </div>
+            <Container className={style.nav} >
+                <img src="/" alt="" className={style.imgLogoNav}/>
+                <div>
+                    <Link href="/login">
+                        
+                    </Link>
+                </div>
+            </Container>
         </>
     );
 };
