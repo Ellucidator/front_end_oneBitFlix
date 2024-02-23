a7216617c78df1b4abd52a78449632ef47975724
Marcelo Leal <andradefj13@hotmail.com>
2024-02-23 16:43:36 -0300

    update
    contrução e estilização da pag de login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,8 @@
 import HeaderGeneric from '@/components/common/headerGenric'
 import styles from './page.module.scss'
 import { Metadata } from 'next'
+import { Container } from 'reactstrap'
+import Footer from '@/components/common/footer'
 
 
 export const metadata:Metadata = {
@@ -13,8 +15,33 @@ const Login = function () {
 
     return (
         <>
-            <main>
+            <main className={styles.main}>
                 <HeaderGeneric logoUrl="/" btnUrl="/register" btnContent="Quero fazer cadastro"/>
+                <Container className='py-5'>
+                    <p className={styles.formTitle}>Bem vindo(a) de volta!</p>
+                    <form action="" className={`d-flex flex-column justify-content-center align-items-center gap-5  ${styles.form}`}>
+                        <div className={`input-group ${styles.formGroup}`}>
+                            <label htmlFor="email" className={`form-label ${styles.label}`}>EMAIL</label>
+                            <input
+                                id='email'
+                                name='email'
+                                type='email'
+                                className={`form-control ${styles.input}`}
+                            />
+                        </div>
+                        <div className={`input-group ${styles.formGroup}`}>
+                            <label htmlFor="password" className={`form-label ${styles.label}`}>SENHA</label>
+                            <input
+                                id='password'
+                                name='password'
+                                type='password'
+                                className={`form-control ${styles.input}`}
+                            />
+                        </div>
+                        <button type="submit" className={`btn btn-outline-secondary ${styles.formBtn}`}>Entrar</button>
+                    </form>
+                </Container>
+                <Footer/>
             </main>
         </>
     )
