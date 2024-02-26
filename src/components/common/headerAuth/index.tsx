d598b99f459b15770c3851b85556becfbd070fb5
Marcelo Leal <andradefj13@hotmail.com>
2024-02-26 18:47:57 -0300

    update
    criação e estilização base do modal

--- src/components/common/headerAuth/index.tsx
+++ src/components/common/headerAuth/index.tsx
@@ -1,24 +1,48 @@
+'use client'
 import { Container } from 'reactstrap'
 import styles from './styles.module.scss'
 import Link from 'next/link'
+import Modal from 'react-modal'
+import { useState } from 'react'
 
 
+const HeaderAuth = function () {
 
-const HeaderAuth = function(){
+    const [modalIsOpen, setIsOpen] = useState(false);
+    function openModal() {
+        setIsOpen(true);
+    }
+    function closeModal() {
+        setIsOpen(false);
+    }
 
-    return(
+    return (
         <>
             <Container className={styles.nav}>
                 <Link href='/'>
-                    <img src="common-image/logoOnebitflix.svg" alt="logoOnebitflix" className={styles.headerLogo}/>
+                    <img src="common-image/logoOnebitflix.svg" alt="logoOnebitflix" className={styles.headerLogo} />
                 </Link>
-                <div className={styles.search}>
-                    <form>
-                        <input name='search' type="search" placeholder='Pesquisar' className={styles.input}/>
-                    </form>
-                    <img src="homeAuth/iconSearch.svg" alt="lupaHeader" className={styles.searchImage} />
+                <div className='d-flex gap-3'>
+                    <div className={styles.search}>
+                        <form>
+                            <input name='search' type="search" placeholder='Pesquisar' className={styles.input} />
+                        </form>
+                        <img src="homeAuth/iconSearch.svg" alt="lupaHeader" className={styles.searchImage} />
+                    </div>
+                    <p onClick={openModal} className={styles.userProfile}>AB</p>
                 </div>
-                <p className={styles.userProfile}>AB</p>
+                <Modal
+                    isOpen={modalIsOpen}
+                    onRequestClose={closeModal}
+                    className={styles.modal}
+                    shouldCloseOnEsc={true}
+                    overlayClassName={styles.modalOverlay}
+                >
+                    <Link href="/profile">
+                        <p className={styles.modalLink}>Meus Dados</p>
+                    </Link>
+                    <p className={styles.modalLink}>Sair</p>
+                </Modal>
             </Container>
         </>
     )
